33e65268a8f53d1c257318e38ed297643c3b65fd
dong.qi <42195979+qidongde@users.noreply.github.com>
2023-09-01 22:05:25 -0500

    Add files via upload

---
+++ mat2cvs.py
@@ -0,0 +1,23 @@
+from scipy.io import loadmat
+import pandas as pd
+
+filename = 'Converted_data_for_AE'
+raw_data = loadmat(filename)
+
+feature = raw_data['Input_time_series']
+Target_CCN = raw_data['Target_CCN']
+time_traj = raw_data['time_traj']
+
+
+def mat_to_cvs():
+    for idx in range(len(time_traj)):
+        if str(Target_CCN[idx].item()) == 'nan':
+            filename = str(time_traj[idx][-1]) + '.csv'
+            pd.DataFrame(feature[idx][0]).to_csv('./dataset/no_target/' + filename)
+        else:
+            filename = str(Target_CCN[idx].item()) + '_' + str(time_traj[idx][-1]) + '.csv'
+            pd.DataFrame(feature[idx][0]).to_csv('./dataset/with_target/' + filename)
+
+
+if __name__ == '__main__':
+    mat_to_cvs()
