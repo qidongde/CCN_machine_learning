7392269a67a4aea3ec52847e9d92507f3e3716e3
dong.qi <42195979+qidongde@users.noreply.github.com>
2023-09-18 21:34:00 -0500

    mean_std_faster_calcu_method

--- mat2cvs.py
+++ mat2cvs.py
@@ -31,15 +31,25 @@ def time_traj_form():
 
 def norm_calcu_fuc():
     np_tmp = feature[0][0]
-    for i in range(1, 30000):
-        np_tmp = np.concatenate([np_tmp, feature[i][0]], axis=1)
-        if i % 2000 == 0:
-            print(f'************num:{i}************')
-    x_mean_value = np.mean(np_tmp, axis=1)
-    x_std_value = np.std(np_tmp, axis=1)
-    x_std_value[x_std_value == 0] = 1
+    # for i in range(1, 30000):
+    #     np_tmp = np.concatenate([np_tmp, feature[i][0]], axis=1)
+    #     if i % 2000 == 0:
+    #         print(f'************num:{i}************')
+    # x_mean_value = np.mean(np_tmp, axis=1)
+    # x_std_value = np.std(np_tmp, axis=1)
 
-    return x_mean_value, x_std_value
+    feature_df = pd.DataFrame(list([list(feature[i]) for i in range(feature.size)]))
+    mean_list = []
+    std_list = []
+    for i in range(25):
+        list_i = []
+        for j in range(68016):
+            list_i.extend(list(feature_df[i][j]))
+        array_i = np.array(list_i)
+        mean_list.append(np.mean(array_i))
+        std_list.append(np.std(array_i))
+
+    return mean_list, std_list
 
 
 if __name__ == '__main__':
