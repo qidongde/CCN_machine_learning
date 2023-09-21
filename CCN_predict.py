cfbe04e2030f4309b67421d3b1c986e16efa3d05
dong.qi <42195979+qidongde@users.noreply.github.com>
2023-09-20 22:32:59 -0500

    model_fine_tuning

--- CCN_predict.py
+++ CCN_predict.py
@@ -1,5 +1,5 @@
 import math
-# from scipy.io import loadmat
+from scipy.io import loadmat
 
 # from sklearn.preprocessing import StandardScaler
 import random
@@ -20,11 +20,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ratio = 0.15
 data_path = r'./dataset/with_target'
 batch_size = 64
-hidden_size = 100
-dropout_ratio = 0.4
+hidden_size = 200
+dropout_ratio = 0.3
 num_layers = 1
-mylr = 0.005
-epochs = 20
+mylr = 0.0002
+epochs = 50
+
+
+# weight_decay = 1e-2
 
 
 def train_test_split_func(full_list, ratio, shuffle=True):
@@ -40,19 +43,33 @@ def train_test_split_func(full_list, ratio, shuffle=True):
 
 
 def time_traj_list():
-    time_traj_df = pd.read_csv('./dataset/time_traj.csv')
-    date_str_list = []
-    date_dic = {}
-    for idx in range(len(time_traj_df)):
-        date_str = str(time_traj_df.iloc[idx][1]) + str(time_traj_df.iloc[idx][2]) + str(time_traj_df.iloc[idx][3])
-        time_str = str(time_traj_df.iloc[idx][-1])
-        date_str_list.append(date_str)
-        date_dic[time_str] = date_str
-        if idx % 20000 == 0:
-            print(f'************Reading data number: {idx}************')
-    date_str_list = list(set(date_str_list))
-    train_date_list, test_date_list = train_test_split_func(date_str_list, ratio, shuffle=True)
-    return train_date_list, test_date_list, date_dic
+    # time_traj_df = pd.read_csv('./dataset/time_traj.csv')
+    # date_str_list = []
+    # date_dic = {}
+    # for idx in range(len(time_traj_df)):
+    #     date_str = str(time_traj_df.iloc[idx][1]) + str(time_traj_df.iloc[idx][2]) + str(time_traj_df.iloc[idx][3])
+    #     time_str = str(time_traj_df.iloc[idx][-1])
+    #     date_str_list.append(date_str)
+    #     date_dic[time_str] = date_str
+    #     if idx % 20000 == 0:
+    #         print(f'************Reading data number: {idx}************')
+    # date_str_list = list(set(date_str_list))
+    # train_date_list, test_date_list = train_test_split_func(date_str_list, ratio, shuffle=True)
+    # return train_date_list, test_date_list, date_dic
+    filenames = os.listdir(data_path)
+    day_list = []
+    # num_count_1 = 0
+    for file_name in filenames:
+        day_int = file_name.split('_')[-1].split('.')[0]
+        day_list.append(day_int)
+        # num_count_1 += 1
+        # if num_count_1 % 5000 == 0:
+        #     print(f'************Reading data step1 number: {num_count_1}************')
+    day_list = list(set(day_list))
+    train_date_list, test_date_list = train_test_split_func(day_list, ratio, shuffle=True)
+    print(f'train_day_num:{len(train_date_list)},test_day_num:{len(test_date_list)}')
+
+    return train_date_list, test_date_list
 
 
 def norm_calcu_fuc():
@@ -123,15 +140,21 @@ def norm_calcu_fuc():
 
 def my_getdata():
     x_mean_value, x_std_value = norm_calcu_fuc()
-    train_date_list, test_date_list, date_dic = time_traj_list()
+    # train_date_list, test_date_list, date_dic = time_traj_list()
+    train_date_list, test_date_list = time_traj_list()
     filenames = os.listdir(data_path)
     train_data_list = []
     test_data_list = []
+    num_count_2 = 0
     for file_name in filenames:
-        if date_dic[file_name.split('_')[1].strip('.csv')] in train_date_list:
+        # if date_dic[file_name.split('_')[1].strip('.csv')] in train_date_list:
+        if file_name.split('_')[-1].split('.')[0] in train_date_list:
             train_data_list.append(file_name)
         else:
             test_data_list.append(file_name)
+        num_count_2 += 1
+        if num_count_2 % 10000 == 0:
+            print(f'************Reading data number: {num_count_2}************')
     train_data_pairs = []
     test_data_pairs = []
 
@@ -152,9 +175,12 @@ def my_getdata():
         test_x = (test_x.transpose() - x_mean_value) / x_std_value
         test_x_y = [test_x.transpose(), test_y]
         test_data_pairs.append(test_x_y)
+    print(f'train_set_num:{len(train_data_pairs)},test_set_num:{len(test_data_pairs)}')
+
     return train_data_pairs, test_data_pairs
 
 
+start_time = time.time()
 train_data_pairs, test_data_pairs = my_getdata()
 
 
@@ -186,16 +212,20 @@ class EncoderLSTM(nn.Module):
         self.num_layers = num_layers
         self.batch_size = batch_size
 
-        self.lstm = nn.LSTM(input_size, self.hidden_size, num_layers, batch_first=True, dropout=dropout_ratio)
-        self.linear = nn.Linear(self.hidden_size, output_size)
+        self.lstm = nn.LSTM(input_size, self.hidden_size, num_layers, batch_first=True)
+        self.linear1 = nn.Linear(self.hidden_size, output_size)
+        self.linear2 = nn.Linear(self.hidden_size, output_size)
         self.dropout = nn.Dropout(p=dropout_ratio)
 
     def forward(self, input, hidden, c):
-        input = input.view(batch_size, 121, 25)
+        input = input.view(-1, 121, 25)
         rr, (hn, c) = self.lstm(input, (hidden, c))
         rr = rr[:, -1, :]
-        rr = self.linear(rr)
-        # rr = self.dropout(rr)
+        rr = self.dropout(rr)
+        rr = self.linear1(rr)
+        hn = hn[-1, :, :]
+        hn = self.linear2(hn)
+
         return rr, hn, c
 
     def inithiddenAndC(self):
@@ -204,7 +234,6 @@ class EncoderLSTM(nn.Module):
 
 
 def Train():
-    start_time = time.time()
     Encoder = EncoderLSTM(25, hidden_size, 1, num_layers, batch_size).to(device)
     myadam_encode = torch.optim.Adam(Encoder.parameters(), lr=mylr)
     mse_loss = nn.MSELoss()
@@ -222,28 +251,34 @@ def Train():
         test_dataset = DataPairsDataset(test_data_pairs)
         train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
         test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-        with torch.no_grad():
-            for test_item, (test_x, test_y) in enumerate(test_dataloader, start=1):
-                test_hidden, test_c = Encoder.inithiddenAndC()
-                test_predict, test_hidden, test_c = Encoder(test_x, test_hidden, test_c)
-                test_loss = mse_loss(test_predict.squeeze(), test_y)
 
-                test_iter_num = test_iter_num + 1
-                test_total_loss = test_total_loss + test_loss.item()
-
+        Encoder.train()
         for train_item, (train_x, train_y) in enumerate(tqdm(train_dataloader), start=1):
             train_hidden, train_c = Encoder.inithiddenAndC()
             train_output, train_hidden, train_c = Encoder(train_x, train_hidden, train_c)
             train_output = train_output.to(device)
+            # train_hidden = train_hidden.to(device)
 
             Encoder.zero_grad()
             train_loss = mse_loss(train_output.squeeze(), train_y)
+            # train_loss = mse_loss(train_hidden.squeeze(), train_y)
             myadam_encode.zero_grad()
             train_loss.backward()
             myadam_encode.step()
 
             train_iter_num = train_iter_num + 1
             train_total_loss = train_total_loss + train_loss.item()
+
+        Encoder.eval()
+        with torch.no_grad():
+            for test_item, (test_x, test_y) in enumerate(test_dataloader, start=1):
+                test_hidden, test_c = Encoder.inithiddenAndC()
+                test_predict, test_hidden, test_c = Encoder(test_x, test_hidden, test_c)
+                test_loss = mse_loss(test_predict.squeeze(), test_y)
+                # test_loss = mse_loss(test_hidden.squeeze(), test_y)
+
+                test_iter_num = test_iter_num + 1
+                test_total_loss = test_total_loss + test_loss.item()
 
         train_mse_loss = train_total_loss / train_iter_num
         test_mse_loss = test_total_loss / test_iter_num
@@ -252,14 +287,13 @@ def Train():
         test_rmse_loss = math.sqrt(test_mse_loss)
         train_avg_loss_list.append(train_rmse_loss)
         test_avg_loss_list.append(test_rmse_loss)
+        time.sleep(0.2)
         print('Epoch:', epoch_idx, "Train RMSELoss:", train_rmse_loss, "|", "Test RMSELoss:", test_rmse_loss)
 
     torch.save(Encoder.state_dict(), './model/lstm_method_%s.pth' % time.time())
     pd.DataFrame(train_avg_loss_list).to_csv('train_avg_loss_list.csv')
     pd.DataFrame(test_avg_loss_list).to_csv('test_avg_loss_list.csv')
-    end_time = time.time()
-    time_consuming = end_time - start_time
-    print(f'time consuming: {time_consuming:.2f}s')
+
     plt.figure()
     plt.plot(train_avg_loss_list, label='train_loss', color='b')
     plt.plot(test_avg_loss_list, label='test_loss', color='g')
@@ -268,6 +302,9 @@ def Train():
     plt.ylabel('Loss')
     plt.savefig('./LSTM_RMSE_loss.png')
     plt.show()
+    end_time = time.time()
+    time_consuming = end_time - start_time
+    print(f'time consuming: {time_consuming:.2f}s')
 
 
 if __name__ == '__main__':
